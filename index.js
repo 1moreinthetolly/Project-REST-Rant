2ad2e6da494ec3ca15fc0311fd2e31e7b4058a31
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-01-19 15:20:01 -0500

    Adding 404 page route

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+
 const express = require('express')
 const app = express()
 
@@ -5,4 +7,8 @@ app.get('/', function (req, res) {
     res.send('hello world')
 })
 
-app.listen(3001)
+app.get('*', (req, res) => {
+    res.status(404).send('<h1>404 Page</h1>')
+})
+
+app.listen(process.env.PORT)
