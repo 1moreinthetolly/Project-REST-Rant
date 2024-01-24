c55e0022dc46a0ceda6daf1a0b58b1525b7d4bdb
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-01-23 22:53:34 -0500

    Adding Bootstrap grid to index page

--- controllers/places.js
+++ controllers/places.js
@@ -6,13 +6,17 @@ router.get('/', (req, res) => {
         city: 'Seattle',
         state: 'WA',
         cuisines: 'Thai, Pan-Asian',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/jason-leung-poI7DelFiVA-unsplash.jpg',
+        creditName: 'Jason Leung',
+        credit: 'https://unsplash.com/photos/photo-of-pub-set-in-room-during-daytime-poI7DelFiVA#:~:text=Photo%20by-,Jason%20Leung,-on%20Unsplash'
       }, {
         name: 'Coding Cat Cafe',
         city: 'Phoenix',
         state: 'AZ',
         cuisines: 'Coffee, Bakery',
-        pic: 'http://placekitten.com/250/250'
+        pic: '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg',
+        creditName: 'Jay Wennington',
+        credit: 'https://unsplash.com/photos/dish-on-white-ceramic-plate-N_Y88TWmGwA#:~:text=Photo%20by-,Jay%20Wennington,-on%20Unsplash'
       }]
       
     res.render('places/index', {places})
--- views/home.jsx
+++ views/home.jsx
@@ -6,6 +6,14 @@ function home () {
     <Def>
         <main>
             <h1>HOME</h1>
+            <div>
+                <img src="/images/sunorwind-PgkGsxjvGB4-unsplash.jpg" alt="Slice of Lasagna" height="500px" width="500"/>
+                <div>
+                Photo by <a href="https://unsplash.com/@sunorwind?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">sunorwind</a> on <a href="https://unsplash.com/photos/cooked-food-with-sauce-PgkGsxjvGB4?utm_content=creditCopyText&utm_medium=referral&utm_source=unsplash">Unsplash</a>
+  
+              </div>
+            </div>
+
             <a href="/places">
   <button className="btn-primary">Places Page</button>
 </a>
