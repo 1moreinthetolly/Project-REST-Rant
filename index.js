313d588930ec76d9c29ba6e52e353dc1b2770ceb
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-01-24 17:22:58 -0500

    Form data is getting through

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
+//Modules and Globals
 require('dotenv').config()
 
 const express = require('express')
 const app = express()
 
+//Express Settings
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 
 app.use('/places', require('./controllers/places'))
+app.use(express.urlencoded({ extended: true }))
+
 
 app.get('/', function (req, res) {
     res.render('home')
