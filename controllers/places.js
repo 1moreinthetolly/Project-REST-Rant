c5953bb13191b9c20dce47e125edd7a365d7f6d6
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-01-25 22:45:34 -0500

    Added the edit form

--- controllers/places.js
+++ controllers/places.js
@@ -18,6 +18,18 @@ router.get('/:id', (req, res) => {
   }
 })
 
+router.get('/:id/edit', (req, res) => {
+  let id = Number(req.params.id)
+  if (isNaN(id)) {
+    res.render('error404')
+  }
+  else if (!places[id]) {
+    res.render('error404')
+  }  
+  else {
+    res.render('places/edit', {place: places[id], id })
+  }
+})
 
 router.get('/', (req, res) => { 
     res.render('places/index', {places})
