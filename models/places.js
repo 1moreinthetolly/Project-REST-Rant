cc5d5e5c6500fd9bad0db65e3a68c279b0158ffc
Mohamed Tarakan <moetarakan@mohameds-air.lan>
2024-01-30 21:13:51 -0500

    Rest Rant pt7: Mongoose

--- models/places.js
+++ models/places.js
@@ -1,17 +1,36 @@
-module.exports = [{
-  name: 'H-Thai-ML',
-  city: 'Seattle',
-  state: 'WA',
-  cuisines: 'Thai, Pan-Asian',
-  pic: '/images/jason-leung-poI7DelFiVA-unsplash.jpg',
-  creditName: 'Jason Leung',
-  credit: 'https://unsplash.com/photos/photo-of-pub-set-in-room-during-daytime-poI7DelFiVA#:~:text=Photo%20by-,Jason%20Leung,-on%20Unsplash'
-}, {
-  name: 'Coding Cat Cafe',
-  city: 'Phoenix',
-  state: 'AZ',
-  cuisines: 'Coffee, Bakery',
-  pic: '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg',
-  creditName: 'Jay Wennington',
-  credit: 'https://unsplash.com/photos/dish-on-white-ceramic-plate-N_Y88TWmGwA#:~:text=Photo%20by-,Jay%20Wennington,-on%20Unsplash'
-}]
+const mongoose = require('mongoose')
+
+const placeSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  pic: String,
+  cuisines: { type: String, required: true},
+  city: { type: String, default: 'Anytown'},
+  state: { type: String, default: 'USA'},
+  founded: Number
+})
+
+module.exports = mongoose.model('Place', placeSchema)
+
+
+
+
+
+
+
+//module.exports = [{
+//  name: 'H-Thai-ML',
+  //city: 'Seattle',
+  //state: 'WA',
+  //cuisines: 'Thai, Pan-Asian',
+  //pic: '/images/jason-leung-poI7DelFiVA-unsplash.jpg',
+  //creditName: 'Jason Leung',
+  //credit: 'https://unsplash.com/photos/photo-of-pub-set-in-room-during-daytime-poI7DelFiVA#:~:text=Photo%20by-,Jason%20Leung,-on%20Unsplash'
+//}, {
+ // name: 'Coding Cat Cafe',
+ // city: 'Phoenix',
+ // state: 'AZ',
+ // cuisines: 'Coffee, Bakery',
+ // pic: '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg',
+ // creditName: 'Jay Wennington',
+ // credit: 'https://unsplash.com/photos/dish-on-white-ceramic-plate-N_Y88TWmGwA#:~:text=Photo%20by-,Jay%20Wennington,-on%20Unsplash'
+//}]
